8a359737851f2ea26a5e89cee908e91559451958
chingutravelandtours <chingutravelandtours@gmail.com>
2026-08-13 01:18:40 +0400

    Create page.tsx

---
+++ app/visa/visadata/page.tsx
@@ -0,0 +1,38 @@
+export const visaData = [
+
+{
+country:"Japan",
+image:"/images/visa/japan.jpg",
+processing:"5-7 working days",
+fee:"PHP 1,000"
+},
+
+{
+country:"Korea",
+image:"/images/visa/korea.jpg",
+processing:"5 working days",
+fee:"PHP 700"
+},
+
+{
+country:"Australia",
+image:"/images/visa/australia.jpg",
+processing:"15-45 working days",
+fee:"PHP 3,000"
+},
+
+{
+country:"USA",
+image:"/images/visa/usa.jpg",
+processing:"5-7 working days",
+fee:"PHP 3,000"
+},
+
+{
+country:"Canada",
+image:"/images/visa/canada.jpg",
+processing:"15-30 working days",
+fee:"PHP 3,000"
+},
+
+];
