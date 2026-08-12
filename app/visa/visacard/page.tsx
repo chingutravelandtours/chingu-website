cfc4e9671c7e2d1024c1686bb5c16e719ac4959e
chingutravelandtours <chingutravelandtours@gmail.com>
2026-08-13 01:21:26 +0400

    Update page.tsx

--- app/visa/visacard/page.tsx
+++ app/visa/visacard/page.tsx
@@ -1,89 +1,82 @@
 type Props = {
-  visa:{
-    country:string;
-    image:string;
-    processing:string;
-    fee:string;
-  }
-}
+  visa: {
+    country: string;
+    image: string;
+    processing: string;
+    fee: string;
+  };
+};
+
+export default function VisaCard({ visa }: Props) {
+  return (
+    <div
+      className="
+        bg-white
+        rounded-2xl
+        overflow-hidden
+        shadow-md
+        border
+        border-gray-100
+        hover:-translate-y-2
+        transition
+      "
+    >
+
+      <img
+        src={visa.image}
+        alt={visa.country}
+        className="
+          w-full
+          h-48
+          object-cover
+        "
+      />
+
+      <div className="p-5">
+
+        <h2 className="
+          text-xl
+          font-bold
+          text-gray-900
+        ">
+          {visa.country}
+        </h2>
+
+        <p className="mt-3 text-sm text-gray-500">
+          Processing Time
+        </p>
+
+        <p className="font-semibold text-gray-800">
+          {visa.processing}
+        </p>
+
+        <p className="mt-3 text-sm text-gray-500">
+          Starting Fee
+        </p>
+
+        <p className="font-bold text-red-600">
+          {visa.fee}
+        </p>
 
 
-export default function VisaCard({visa}:Props){
+        <button
+          className="
+            mt-5
+            w-full
+            bg-red-600
+            text-white
+            py-3
+            rounded-xl
+            font-semibold
+            hover:bg-red-700
+            transition
+          "
+        >
+          Apply Now →
+        </button>
 
-return(
+      </div>
 
-<div className="
-bg-white
-rounded-2xl
-overflow-hidden
-shadow-md
-border
-border-gray-100
-hover:-translate-y-2
-transition
-">
-
-
-<img
-src={visa.image}
-className="
-w-full
-h-48
-object-cover
-"
-/>
-
-
-<div className="p-5">
-
-
-<h2 className="
-text-xl
-font-bold
-text-gray-900
-">
-{visa.country}
-</h2>
-
-
-<p className="mt-3 text-sm text-gray-500">
-Processing:
-</p>
-
-<p className="font-semibold">
-{visa.processing}
-</p>
-
-
-<p className="mt-3 text-sm text-gray-500">
-Starting Fee:
-</p>
-
-<p className="font-bold text-red-600">
-{visa.fee}
-</p>
-
-
-<button
-className="
-mt-5
-w-full
-bg-red-600
-text-white
-py-3
-rounded-xl
-font-semibold
-hover:bg-red-700
-"
->
-Apply Now →
-</button>
-
-
-</div>
-
-</div>
-
-)
-
+    </div>
+  );
 }
