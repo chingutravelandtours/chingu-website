12d0a128dfb8820c5312280f43d0c16299e3dd04
chingutravelandtours <chingutravelandtours@gmail.com>
2026-08-15 20:26:50 +0400

    Update page.tsx

--- app/china-tours/page.tsx
+++ app/china-tours/page.tsx
@@ -631,6 +631,28 @@ const packages: Package[] = [
   slots: 20,
   deposit: 500 * USD_TO_PHP,
 },
+
+  {
+  title: "GuangZhou",
+  subtitle: "Land Tour Purefun",
+  duration: "4D3N",
+  dates: "Flexible Schedule (Peak dates are subject to surcharge)",
+  city: "MNL",
+  price: 349 * USD_TO_PHP,
+  minimum: 4,
+  deposit: 200 * USD_TO_PHP,
+},
+
+{
+  title: "XinJiang",
+  subtitle: "Land Tour Purefun",
+  duration: "4D3N",
+  dates: "Flexible Schedule (Peak dates are subject to surcharge)",
+  city: "MNL",
+  price: 849 * USD_TO_PHP,
+  minimum: 4,
+  deposit: 500 * USD_TO_PHP,
+},
 ];
 
 export default function ChinaToursPage() {
