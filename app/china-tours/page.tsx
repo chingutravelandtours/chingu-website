fc836d0b336cf3e5545f6e35ba67e59d5714c79b
chingutravelandtours <chingutravelandtours@gmail.com>
2026-08-15 16:41:09 +0400

    Update page.tsx

--- app/china-tours/page.tsx
+++ app/china-tours/page.tsx
@@ -395,7 +395,14 @@ Please send me the complete package details. Thank you!`
         </div>
 
         {/* CHINA PACKAGE GRID */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
+        <div className="
+  grid
+  grid-cols-1
+  sm:grid-cols-2
+  md:grid-cols-3
+  lg:grid-cols-5
+  gap-5
+">
 
           {packages.map((pkg, index) => (
             <div
