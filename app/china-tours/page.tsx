cc5ed39a3f0a37f20302bc42107e0372774298ac
chingutravelandtours <chingutravelandtours@gmail.com>
2026-08-15 13:31:00 +0400

    Update page.tsx

--- app/china-tours/page.tsx
+++ app/china-tours/page.tsx
@@ -196,6 +196,17 @@ const packages: Package[] = [
   title: "Chongqing + Zhangjiajie + Shanghai",
   subtitle: "Shopping",
   duration: "7D6N",
+  dates: "Nov. 28 – Dec. 4, 2026",
+  city: "MNL",
+  price: 888 * USD_TO_PHP,
+  slots: 20,
+  deposit: 400 * USD_TO_PHP,
+},
+
+  {
+  title: "Chongqing + Zhangjiajie + Shanghai",
+  subtitle: "Shopping",
+  duration: "7D6N",
   dates: "Dec. 21–27, 2026",
   city: "MNL",
   price: 1033 * USD_TO_PHP,
