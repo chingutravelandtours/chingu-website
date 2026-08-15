24e0062d7de1c4d39a8ae501c10c76a5a9baf406
chingutravelandtours <chingutravelandtours@gmail.com>
2026-08-15 15:15:08 +0400

    Create palawanHotels.ts

---
+++ app/hotel/palawanHotels.ts
@@ -0,0 +1,31 @@
+export const palawanHotels = [
+  {
+    name: "El Nido Beach Resort",
+    location: "El Nido, Palawan",
+    country: "Philippines",
+    price: "$150 / Night",
+    image: "/hotels/palawan/elnido.jpg",
+    rating: "★★★★★",
+    room: "Deluxe Room",
+  },
+
+  {
+    name: "Coron Island Resort",
+    location: "Coron, Palawan",
+    country: "Philippines",
+    price: "$120 / Night",
+    image: "/hotels/palawan/coron.jpg",
+    rating: "★★★★★",
+    room: "Premium Villa",
+  },
+
+  {
+    name: "Puerto Princesa Hotel",
+    location: "Puerto Princesa, Palawan",
+    country: "Philippines",
+    price: "$80 / Night",
+    image: "/hotels/palawan/puerto-princesa.jpg",
+    rating: "★★★★☆",
+    room: "Standard Room",
+  },
+];
