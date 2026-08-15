9054dfc077da0408ab5861e02c3acdb70b45e075
chingutravelandtours <chingutravelandtours@gmail.com>
2026-08-15 20:43:55 +0400

    Update page.tsx

--- app/china-tours/page.tsx
+++ app/china-tours/page.tsx
@@ -783,7 +783,6 @@ Please send me the complete package details. Thank you!`
   };
 
   return (
-   <main className="min-h-screen bg-gray-50 py-12 px-5">
      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
        
         {/* HEADER */}
