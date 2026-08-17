e40d7025a75d1aaa1d8c178a5351c54e516880ec
chingutravelandtours <chingutravelandtours@gmail.com>
2026-08-17 13:48:23 +0400

    Update TourPackages.tsx

--- components/TourPackages.tsx
+++ components/TourPackages.tsx
@@ -32,7 +32,7 @@ const destinations = [
   subtitle: "Taiwan Packages",
   description: "Explore our available Taiwan tour packages.",
   icon: "🇹🇼",
-  href: "/taiwan-tours",
+  href: "/taiwan-tour",
 },
 {
   title: "South Korea",
