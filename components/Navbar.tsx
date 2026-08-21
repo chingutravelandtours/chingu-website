48a8d95676d9a6473c15d4fa36b8a2750c9a3067
chingutravelandtours <chingutravelandtours@gmail.com>
2026-08-21 17:55:52 +0400

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -70,14 +70,11 @@ export default function Navbar() {
 
 
         <Link
-          href="/contact"
-          className="
-            hover:text-red-600
-            transition
-          "
-        >
-          Contact Us
-        </Link>
+  href="/gael-ai"
+  className="hover:text-red-600 transition"
+>
+  Contact Us
+</Link>
 
 
 
