d9c98c3d2aa7e7e97d77a7b0b2f2aaaf9c895fe1
chingutravelandtours <chingutravelandtours@gmail.com>
2026-08-30 12:56:16 +0400

    Update page.tsx

--- app/esim/page.tsx
+++ app/esim/page.tsx
@@ -1,16 +1,15 @@
-
 import EsimPlans from "@/components/EsimPlans";
 
 export default function EsimPage() {
   return (
     <main className="bg-white">
 
-      {/* eSIM HERO IMAGE */}
-      <section className="relative w-full overflow-hidden">
+      {/* eSIM HERO */}
+      <section className="w-full overflow-hidden">
         <img
-          src="/images/esim.jpg"
+          src="/images/hero/esim.jpeg"
           alt="Chingu Travel and Tours eSIM Travel Plans"
-          className="w-full h-auto object-cover"
+          className="w-full h-auto block"
         />
       </section>
 
