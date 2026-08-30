26b5839b7da3781ba2e0152c64e0ae76c5e2dd85
chingutravelandtours <chingutravelandtours@gmail.com>
2026-08-30 14:16:54 +0400

    Update page.tsx

--- app/esim/page.tsx
+++ app/esim/page.tsx
@@ -5,13 +5,14 @@ export default function EsimPage() {
     <main className="bg-white">
 
       {/* eSIM HERO */}
-      <<section className="w-full bg-white overflow-hidden">
-  <img
-    src="/images/hero/esim (10).png"
-    alt="Chingu Travel and Tours eSIM Travel Plans"
-    className="w-full h-auto block"
-  />
-</section>
+      <section className="w-full bg-white overflow-hidden">
+        <img
+          src="/images/hero/esim (10).png"
+          alt="Chingu Travel and Tours eSIM Travel Plans"
+          className="w-full h-auto block"
+        />
+      </section>
+
       {/* eSIM PLANS */}
       <EsimPlans />
 
