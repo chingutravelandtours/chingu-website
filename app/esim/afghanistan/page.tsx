ca3556e0f46efc41d0672d32d1f7862cac26c6f2
chingutravelandtours <chingutravelandtours@gmail.com>
2026-09-01 11:57:13 +0400

    Create page.tsx

---
+++ app/esim/afghanistan/page.tsx
@@ -0,0 +1,9 @@
+import EsimPlans from "@/components/EsimPlans";
+
+export default function AfghanistanEsimPage() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <EsimPlans />
+    </main>
+  );
+}
