b3c15ef918386e9186566acd9aefedfd3c509c73
chingutravelandtours <chingutravelandtours@gmail.com>
2026-09-14 10:01:35 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import Services from "../components/Services";
 import TourPackages from "../components/TourPackages";
 
 export default function Home() {
@@ -8,7 +7,6 @@ export default function Home() {
     <>
       <Navbar />
       <Hero />
-      <Services />
       <TourPackages />
     </>
   );
