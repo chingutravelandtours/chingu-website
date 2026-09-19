4b16f7a702355186a8d0c729619829be1758413e
chingutravelandtours <chingutravelandtours@gmail.com>
2026-09-20 00:50:48 +0400

    Remove TourPackages import from Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import TourPackages from "../components/TourPackages";
 import Services from "../components/Services";
 
 export default function Home() {
